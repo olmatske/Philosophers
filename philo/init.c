b47c794e91f7c7df3f192cbe22b720d0e43a25bd
Olga Matskevich <olmatske@2-B-6.42heilbronn.de>
2026-02-02 18:06:15 +0100

    working on forks

--- philo/init.c
+++ philo/init.c
@@ -6,7 +6,7 @@
 /*   By: olmatske <olmatske@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/27 18:25:35 by olmatske          #+#    #+#             */
-/*   Updated: 2026/02/02 14:36:35 by olmatske         ###   ########.fr       */
+/*   Updated: 2026/02/02 16:39:18 by olmatske         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,26 +19,36 @@
 // 	pthread_create()
 // }
 
-// void	init_forks(t_philo *philo, int id, int number_of_philos)
-// {
-// 	if (!(number_of_philos % 2))
-// 	{
-// 		philo[id].lfork = 
-// 	}
-// }
+void	init_forks(t_philo *philo, int id, int number_of_philos)
+{
+	if (!(number_of_philos % 2))
+	{
+		philo[id].lfork = 
+	}
+}
 
 t_table	*init_table(char **argv, t_philo *philo, t_table *table, int meals)
 {
+	int	i;
+
+	i = 0;
 	table = malloc(sizeof(t_table));
 	
 	table->philos = philo;
+	while (i < ft_atoi(argv[1]))
+	{
+		pthread_mutex_init(&table->forks[i], NULL);
+	}
 	// table->forks = forks;
 	table->ttd = ft_atol(argv[2]);
 	table->tte = ft_atol(argv[3]);
 	table->tts = ft_atol(argv[4]);
 	table->nom = meals;
-	
 
+	printf("\nTABLE:\nttd: %lu\n", table->ttd);
+	printf("tte: %lu\n", table->tte);
+	printf("tts: %lu\n", table->tts);
+	printf("nom: %d\n", table->nom);
 	// init_philo(number_of_philos, philo);
 	return (table);
 }
@@ -50,9 +60,11 @@ t_philo	*init_philo(int number_of_philos, t_philo *philo, t_table *table)
 	id = 0;
 	philo = malloc(sizeof(t_philo) * number_of_philos);
 	memset(philo, 0, sizeof(t_philo) * number_of_philos);
+	printf("\nPHILO:\n");
 	while (id < number_of_philos)
 	{
 		philo[id].index = id;
+		
 		philo[id].meal_count = 0;
 		philo[id].time_since_eaten = 0;
 		philo[id].tss = 0;
